d8da920998fe6470a71eab8bd78b02374d760c3b
Sinisa Abramovic <sinisha.abramovich@gmail.com>
2023-03-28 18:25:11 +0200

    fix

--- src/Crypto/AESCipher.hpp
+++ src/Crypto/AESCipher.hpp
@@ -23,13 +23,6 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <boost/archive/iterators/binary_from_base64.hpp>
-#include <boost/archive/iterators/transform_width.hpp>
-#include <boost/archive/iterators/remove_whitespace.hpp>
-#include <boost/archive/iterators/ostream_iterator.hpp>
-#include <boost/archive/iterators/base64_from_binary.hpp>
-#include <boost/archive/iterators/transform_width.hpp>
-#include <boost/archive/iterators/ostream_iterator.hpp>
 
 #include <iostream>
 #include <memory>
